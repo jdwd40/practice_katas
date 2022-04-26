467ac5dd270aee153b218060d712f092eb051f7b
jdwd40 <jd@jdwd40.com>
2022-04-26 18:51:37 +0100

    fix tests and fix largest()

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,6 @@
-const smallest = require('../index');
-const largest = require('../index');
+const {smallest, largest} = require('../index');
 
-const numberData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numberData = [1, 0, 2, 3, 56, 5, 66, 7, 8, 99, 10];
 
 describe('Smallest and Largest Array', () => {
   test('returns an empty array when smallest() is given an empty array', () => {
@@ -10,16 +9,16 @@ describe('Smallest and Largest Array', () => {
   test('returns an empty array when smallest() is sent an empty argument', () => {
     expect(smallest()).toEqual([]);
   });
-  test('returns the largest number given an array when largest() is passed an array', () => {
-    expect(largest(numberData)).toEqual(1);
+  test('returns the smallest number given an array when smallest() is passed an array', () => {
+    expect(smallest(numberData)).toEqual(0);
   });
-  test('returns an empty array when largest() is given an empty array', () => {
+  test('returns an empty array when larest() is given an empty array', () => {
     expect(largest([])).toEqual([]);
   });
   test('returns an empty array when largest() is sent an empty argument', () => {
     expect(largest()).toEqual([]);
   });
-  test('returns the smallest number given an array when smallest is passed an array', () => {
-    expect(smallest(numberData)).toEqual(1);
+  test('returns the largest number given an array when smallest is passed an array', () => {
+    expect(largest(numberData)).toEqual(99);
   });
 });
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const smallest = (arr) => {
   if (arr === undefined || arr.length === 0) return [];
+  console.log('inside smallest()')
   // find smallest number in array
   return Math.min(...arr);
 };
 
-const largest = (arr) => {
-  if (arr === undefined || arr.length === 0) return [];
-  return Math.max(...arr);
+const largest = (arr1) => {
+  if (arr1 === undefined || arr1.length === 0) return [];
+  console.log('inside largest()');
+  //let max = Math.max.apply(null, arr);
+  return Math.max.apply(null, arr1);
 };
 
-module.exports = smallest, largest;
+
+// sort thu array arrange it from smallest to largest
+// select the element thats being asked for from the argument
+module.exports ={smallest, largest};
